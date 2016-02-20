8fe39b638db479acb9e2fce896de8c308b64410f
sam <samijoniramezani12@gmail.com>
2016-02-20 09:15:29 -0800

    Create SUDO.lua

---
+++ plugins/SUDO.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+  return [[
+  ߑ哵doers of Infernal : 
+  ߔ?ArashInfernal 109722284ߑ加 ߔ?Mustafaflux 171604508 ߑ堊  ߔ?digitalboys  99743635 ߑ加 ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^/sudoers$",
+    "^!sudoers$",
+    "^sudoers$",
+  },
+  run = run
+}
+end
