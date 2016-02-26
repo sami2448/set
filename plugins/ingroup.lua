fb7322eaf85fe48a02b4e70064ed87632275311e
sam <samijoniramezani12@gmail.com>
2016-02-26 02:41:42 -0800

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -105,7 +105,7 @@ local function check_member_modadd(cb_extra, success, result)
         group_type = 'Group',
         moderators = {},
         set_owner = member_id ,
-        settings = {
+        ⚙>Group Settings: = {
           set_name = string.gsub(msg.to.print_name, '_', ' '),
           lock_name = 'yes',
           lock_photo = 'no',
